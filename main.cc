9c980aa66d2da4417c56996e0617813ccdb0673a
Isaac Vitorino Lopes Alves <129021004+ivtrL@users.noreply.github.com>
2024-08-01 00:18:02 -0300

    added dotenv to dependencies

--- main.cc
+++ main.cc
@@ -1,11 +1,10 @@
 #include <drogon/drogon.h>
-int main() {
-    //Set HTTP listener address and port
-    drogon::app().addListener("0.0.0.0", 8080);
-    //Load config file
-    //drogon::app().loadConfigFile("../config.json");
-    //drogon::app().loadConfigFile("../config.yaml");
-    //Run HTTP framework,the method will block in the internal event loop
+#include "include/dotenv.h"
+
+int main()
+{
+    dotenv::init();
+    drogon::app().loadConfigFile("../config.yaml");
     drogon::app().run();
     return 0;
 }
